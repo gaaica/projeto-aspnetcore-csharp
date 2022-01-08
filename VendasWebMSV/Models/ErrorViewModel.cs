abc25a5d8ed1006eb4ec25d693615621d97f6ea2
Gabriel A Camargo <gabriel.aires.camargo@gmail.com>
2022-01-08 12:22:59 -0300

    Adicionar arquivos de projeto.

---
+++ VendasWebMSV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VendasWebMSV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
