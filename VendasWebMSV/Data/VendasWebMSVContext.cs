9f38d65ca80863ec417b1b7cf11d30358b6d144c
Gabriel A Camargo <gabriel.aires.camargo@gmail.com>
2022-01-08 14:33:49 -0300

    Utilizado CRUD scaggolding para a classe Department

---
+++ VendasWebMSV/Data/VendasWebMSVContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWebMSV.Models
+{
+    public class VendasWebMSVContext : DbContext
+    {
+        public VendasWebMSVContext (DbContextOptions<VendasWebMSVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWebMSV.Models.Department> Department { get; set; }
+    }
+}
--- VendasWebMSV/Startup.cs
+++ VendasWebMSV/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using VendasWebMSV.Models;
 
 namespace VendasWebMSV
 {
@@ -33,6 +35,9 @@ namespace VendasWebMSV
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VendasWebMSVContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VendasWebMSVContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
