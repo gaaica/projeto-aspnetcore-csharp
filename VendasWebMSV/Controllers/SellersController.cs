38a390f2fc2b1d2615ea6694fc77e18b8c38aca9
Gabriel A Camargo <gabriel.aires.camargo@gmail.com>
2022-01-09 22:18:00 -0300

    Implementador pagína customizada para erros

--- VendasWebMSV/Controllers/SellersController.cs
+++ VendasWebMSV/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -47,12 +48,12 @@ namespace VendasWebMSV.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Identificador do vendedor não fornecido!"});
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Vendedor não encontrado!" });
             }
             return View(obj);
             
@@ -71,12 +72,12 @@ namespace VendasWebMSV.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Identificador do vendedor não fornecido!" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Vendedor não encontrado!" });
             }
             return View(obj);
         }
@@ -85,12 +86,12 @@ namespace VendasWebMSV.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Identificador do vendedor não fornecido!" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Vendedor não encontrado!" });
             }
             List<Department> departments = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
@@ -103,7 +104,7 @@ namespace VendasWebMSV.Controllers
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Identificador do vendedor incompatível!" });
             }
 
             try
@@ -112,14 +113,22 @@ namespace VendasWebMSV.Controllers
 
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+
+            };
+            return View(viewModel);
         }
     }
 }
--- VendasWebMSV/Models/ViewModels/ErrorViewModel.cs
+++ VendasWebMSV/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace VendasWebMSV.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
