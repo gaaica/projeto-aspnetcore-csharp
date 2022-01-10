5510ca9c20a369b9ccbf052eb8caa1541aad1e2a
Gabriel A Camargo <gabriel.aires.camargo@gmail.com>
2022-01-10 09:50:20 -0300

    implementado busca simples de vendas - em MVC completo

--- VendasWebMSV/Models/SalesRecord.cs
+++ VendasWebMSV/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using VendasWebMSV.Models.Enums;
@@ -8,10 +9,21 @@ namespace VendasWebMSV.Models
 {
     public class SalesRecord
     {
+        [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Display(Name = "Data")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Valor")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
+        [Display(Name = "Status")]
         public SaleStatus Status { get; set; }
+
+        [Display(Name = "Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord()
